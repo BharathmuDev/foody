165cb8a8dac26b18c5a9def83015bf894c7f702a
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-08-19 09:42:02 +0530

    Add Menu component

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 75.00,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 85.00,
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: PedroTechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 150.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 99.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 300.99,
+  },
+];
